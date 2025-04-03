a976ef09a6188b30e4ad862d7db5b45dc07a5719
Luisa-333 <luisa1015luisa@gmail.com>
2025-04-03 16:23:40 +0200

    update

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using  System;
+
+namespace Test {
+    class Program {
+        static void Main() {
+            //for, while und do while
+
+            /* for (byte i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Element: {0}", i);
+            } */
+
+            for (float i = 200; i >= 6; i /= 2)
+            {
+                Console.WriteLine("Element: {0}", i);
+            }
+        }
+    }
+}
